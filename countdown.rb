894af5125cb4471e46396db6530920543cdcfca5
Tammy Lam <lamtammy1515@gmail.com>
2020-01-24 17:36:26 +0000

    Done.

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,18 @@
 #write your code here
 
-def countdown
+def countdown(number)
+  while number > 0 
+  puts "#{number} SECOND(S)!"
+  number -= 1 
+end 
+return "HAPPY NEW YEAR!"
 end
+
+countdown(10)
+
+def countdown_with_sleep(num)
+  sleep(num)
+end 
+
+sleep(5)
+  
